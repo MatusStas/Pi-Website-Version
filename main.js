70b5f90ea67ccb5284b9ba8e0f7bfb4f23cca370
Matus Stas <matusstas.ms@gmail.com>
2020-01-16 22:21:57 +0100

    Animation added

--- main.js
+++ main.js
@@ -4,24 +4,29 @@ function drawPoint(ctx) {
 	var arr = [];
 
 	for (var i = 0; i < Math.pow(10,5); i++) {
-		ctx.beginPath();
-		var x = (Math.random()*1800).toFixed(0);
-		var y = (Math.random()*900).toFixed(0);
-		if (Math.pow(x-900,2)+Math.pow(y-450,2) > Math.pow(450,2)) {
-			ctx.arc(x,y, 0.1, 0, 2 * Math.PI);
-			ctx.strokeStyle = "#ff0000";
-			ctx.stroke();
-			outside++;
-			arr.push([x,y]);
-		} else {
-			ctx.arc(x,y, 0.1, 0, 2 * Math.PI);
-			ctx.strokeStyle = "#000000";
-			ctx.stroke();
-			inside++;
-		}		
-		arr.push([x,y]);
+		setTimeout(() => {
+			ctx.beginPath();
+			var x = (Math.random()*1800).toFixed(0);
+			var y = (Math.random()*900).toFixed(0);
+			if (Math.pow(x-900,2)+Math.pow(y-450,2) > Math.pow(450,2)) {
+				ctx.arc(x,y, 0.3, 0, 2 * Math.PI);
+				ctx.strokeStyle = "#ff0000";
+				ctx.stroke();
+				outside++;
+			} else {
+				ctx.arc(x,y, 0.3, 0, 2 * Math.PI);
+				ctx.strokeStyle = "#000000";
+				ctx.stroke();
+				inside++;
+			}
+
+			var pi = 8*(inside/(inside+outside));
+			var text = document.getElementById("inside").innerHTML = "inside: " + inside.toString(); 
+			var text = document.getElementById("outside").innerHTML = "outside: " + outside.toString(); 
+			var text = document.getElementById("pi").innerHTML = pi.toString(); 
+
+		}, 1);
 	}
-	return [inside, outside];
 }
 
 
@@ -37,15 +42,7 @@ function main() {
 	var c = document.getElementById("canvas");
 	var ctx = c.getContext("2d");
 	drawCircle(ctx);
-	var result = drawPoint(ctx);
-
-	var inside = result[0];
-	var outside = result[1];
-
-	var pi = 8*(inside/(inside+outside));
-	var text = document.getElementById("inside").innerHTML = "inside: " + inside.toString(); 
-	var text = document.getElementById("outside").innerHTML = "outside: " + outside.toString(); 
-	var text = document.getElementById("pi").innerHTML = pi.toString(); 
+	drawPoint(ctx);
 }
 
 main()
